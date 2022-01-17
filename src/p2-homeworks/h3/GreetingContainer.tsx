22b2a3dba0c00e5781687706da5c9e6ef187551d
RinalisDS <denispilyutin@gmail.com>
2022-01-17 20:36:34 +0200

    HW №3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -15,22 +16,38 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
     const [name, setName] = useState<any>('') // need to fix any
     const [error, setError] = useState<any>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setError("")
+        setName(e.currentTarget.value) // need to fix
     }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
+    const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError("")
+        if (e.charCode === 13) {
+            addUser(name)
+        }
+
     }
 
-    const totalUsers = 0 // need to fix
+    const addUser = (name: string) => {
+        if (name.trim()) {
+            alert(`Hello , ${name} !`) // need to fix
+            addUserCallback(name)
+            setName("")
+        } else {
+            setError("name is required")
+        }
+    }
+
+    const totalUsers = users.length // need to fix
 
     return (
         <Greeting
             name={name}
             setNameCallback={setNameCallback}
-            addUser={addUser}
+            addUser={() => addUser(name)} // NADO ZADAT VOPROS
             error={error}
             totalUsers={totalUsers}
+            onKeyPress={(e) => onKeyPress(e)}
         />
     )
 }
